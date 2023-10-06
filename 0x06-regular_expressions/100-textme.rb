980fffb030fa9c0e1c29b5f51c93ec195f802353
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-10-06 12:14:36 +0100

    TextMe

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
-sender, number, flags = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
-puts "#{sender},#{number},#{flags}"
+if ARGV.length == 1
+    sender, number, flags = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
+    puts "#{sender},#{number},#{flags}"
+    exit
 end
