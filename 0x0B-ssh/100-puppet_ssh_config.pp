44ec15e68d2f82b70fed1530ffb42c2134f4622c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-26 22:42:17 +0100

    adding last task 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # puppet file
 
-file_line {'refuse to authenticate using a password':
+file_line {'deniying  to authenticate using a password':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   ensure => present,
 }
 
-file_line {'SSH client configuration using the private key':
+file_line {'using the IdentityFile':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   ensure  => present,
