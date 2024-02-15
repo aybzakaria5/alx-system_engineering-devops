873a90da8bf50fef96f65493b42ee2bf196c0805
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-15 01:46:50 +0100

    fixing the puppet-lint erro

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # replacing the false extention aphpp with php 
 exec { 'fixing wp settings':
     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
-    path => '/usr/bin',
+    path    => '/usr/bin',
 }
